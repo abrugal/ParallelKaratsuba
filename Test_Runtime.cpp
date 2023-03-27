872825fc6183d37f0b41297a4d18d531c3a799be
kitorer <vikkhuang1234@gmail.com>
2023-03-27 01:20:32 -0400

    writeout into csv, im too tired ;-;

--- Test_Runtime.cpp
+++ Test_Runtime.cpp
@@ -4,11 +4,13 @@
 #include "Karatsuba_Uncapped.hpp"
 #include "Karatsuba.hpp"
 #include <cmath>
+#include <fstream>
 using namespace std;
 
 // Test Runtime
 // Numbers of digits that are powers of 2, 2^0, 2^1, 2^2, ... 2 ^ 15?
 // 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768
+
 int ThreadPoolTime(int n);
 int SemaphoreTime(int n);
 int sequentialTime(int n);
@@ -16,14 +18,28 @@ int gradeTime(int n);
 
 int main(void)
 {
+  // file output
+  ofstream csv;
+  ofstream textfile;
+  csv.open("output.csv");
+  textfile.open("output.txt");
+  csv << "n,ThreadPool,Semaphore,Sequential,Grade" << endl;
+
+
   for(int i = 0; i < 16; i++){
     int n = pow(2, i);
-    cout << "n = " << n << endl;
-    cout << "ThreadPool: " << ThreadPoolTime(n) << endl;
-    cout << "Semaphore: " << SemaphoreTime(n) << endl;
-    cout << "Sequential: " << sequentialTime(n) << endl;
-    cout << "Grade: " << gradeTime(n) << endl;
-    cout << endl;
+    int tp = ThreadPoolTime(n);
+    int sem = SemaphoreTime(n);
+    int seq = sequentialTime(n);
+    int grade = gradeTime(n);
+    csv << n << "," << tp << "," << sem << "," << seq << "," << grade << endl;
+    
+    textfile << "n = " << n << endl;
+    textfile << "ThreadPool: " << tp << endl;
+    textfile << "Semaphore: " << sem << endl;
+    textfile << "Sequential: " << seq << endl;
+    textfile << "Grade: " << grade << endl;
+    textfile << endl;
   }
 }
 
