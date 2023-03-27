0e489f4c44bc30249e193b3be30f40c64e3398ac
xhuetter <xjhuetter@yahoo.com>
2023-03-26 20:40:25 -0400

    Updated Test_Correctness.cpp
    Added code evaluating whether karatsuba algorithms return correct results

--- Test_Correctness.cpp
+++ Test_Correctness.cpp
@@ -7,11 +7,38 @@ using namespace std;
 
 // Test Correctness
 // No need to test big numbers
-// Multiple all integers between -1000 and 1000 with each other. 
+// Multiple all integers between -1000 and 1000 with each other.
 // Maybe include a couple of large integers
 
 int main(void)
 {
 
+  // const auto processor_count = std::thread::hardware_concurrency();
+
+  //cout << processor_count;
+
+  for (int i = -1000; i <= 1000; i++)
+  {
+    for (int j = -1000; j <= 1000; j++)
+    {
+      BigInt a(i), b(j);
+
+      BigInt normal = gradeschool(a, b);
+      BigInt seq = Karatsuba::karatsuba(a, b);
+      BigInt sem = Karatsuba_Semaphore::ParallelKaratsuba(a, b);
+      BigInt thr = Karatsuba_ThreadPool::ParallelKaratsuba(a, b);
+
+      if (!(normal == seq && normal == sem && normal == thr)){
+        cout << "FAIL: " << i << " " << j << "\n";
+        return 0;
+      }
+      /*
+      else{
+        cout << "PASS: " << i << " " << j << "\n";
+      }
+      */
+    }
+  }
+  cout << "SUCCESS\n";
   return 0;
 }
